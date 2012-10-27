291340af4408db139060e137dd212aad842d6c00
Aaron Saunders <aaronksaunders@gmail.com>
2012-10-27 12:54:44 -0400

    added global object support

--- app/alloy.js
+++ app/alloy.js
@@ -1,1 +1,6 @@
-var todos = Alloy.createCollection('Todo');
+var G = require("globals");
+G.globalGet("todos");
+
+G.globalSet("todos",Alloy.createCollection('Todo'));
+
+Ti.API.info(' TODOS ' + JSON.stringify(G.globalGet("todos").toJSON(),null,2));
