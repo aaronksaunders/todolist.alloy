3022d92e01d3c46f3e710bee2908cf113ea6a8a0
Aaron Saunders <aaronksaunders@gmail.com>
2012-10-27 14:43:03 -0400

    added sample code snippets to index file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,9 +12,23 @@ todos.fetch({
     add : false,
 });
 
-
 /* HOW TO FETCH A SINGLE ITEM */
 //var todo = Alloy.createModel('Todo', {
 //    id : "38e8aa8f-714d-3634-68b3-c825eb61b9e1"
 //});
-//todo.fetch(); 
+//todo.fetch();
+
+// EXTENDED FUNCTION
+/* HOW TO FETCH A ITEM(s) COMPLETED TODAY*/
+/*
+var todos_today = Alloy.createCollection('Todo');
+todos_today.completedToday({
+    success : function(_m, _r) {
+        Ti.API.info(JSON.stringify(_r, null, 2)); debugger;
+    },
+    error : function(_m, _r) {
+        Ti.API.info(JSON.stringify(_r, null, 2));
+        debugger;
+    }
+})
+*/
